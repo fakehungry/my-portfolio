db3904bef63926861804d3bd54dcf06e766655f2
fakehungry <chaikitr@gmail.com>
2022-11-18 11:31:49 +0700

    create schemas

---
+++ backend-sanity/schemas/project.js
@@ -0,0 +1,46 @@
+export default {
+  name: "project",
+  title: "Project",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      description: "Title of the project",
+      type: "string",
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "summary",
+      title: "Summary",
+      type: "text",
+    },
+    {
+      name: "linkToCode",
+      title: "Link to Code",
+      type: "url",
+    },
+    {
+      name: "linkToWebSite",
+      title: "Link To Web Site",
+      type: "url",
+    },
+    {
+      name: "feature",
+      title: "Features",
+      type: "array",
+      of: [
+        {
+          type: "string",
+        },
+      ],
+    },
+  ],
+};
