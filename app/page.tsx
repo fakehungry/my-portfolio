fb31bd2db4ec8c35515eec9942e25c3e867807e7
fakehungry <chaikitr@gmail.com>
2023-07-21 16:09:47 +0700

    fix error

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
+"use client";
 import Dropdown from "@components/dropdown/Dropdown";
-import { Metadata } from "next";
+// import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Chaikit Ruangsakulpat",
-  description:
-    "Chaikit Ruangsakulpat is a software engineer who loves to build web applications.",
-  creator: "Chaikit Ruangsakulpat",
-  publisher: "Chaikit Ruangsakulpat",
-  authors: [{ name: "Chaikit", url: "https://chaikitr.com" }],
-  generator: "Next.js",
-  applicationName: "ChaikitR's Portfolio",
-  referrer: "origin-when-cross-origin",
-  keywords: ["Next.js", "React", "JavaScript", "TypeScript"],
-};
+// export const metadata: Metadata = {
+//   title: "Chaikit Ruangsakulpat",
+//   description:
+//     "Chaikit Ruangsakulpat is a software engineer who loves to build web applications.",
+//   creator: "Chaikit Ruangsakulpat",
+//   publisher: "Chaikit Ruangsakulpat",
+//   authors: [{ name: "Chaikit", url: "https://chaikitr.com" }],
+//   generator: "Next.js",
+//   applicationName: "ChaikitR's Portfolio",
+//   referrer: "origin-when-cross-origin",
+//   keywords: ["Next.js", "React", "JavaScript", "TypeScript"],
+// };
 
 const links = [
   {
